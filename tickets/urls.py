6df4c77336be289b03382b7a13bee2224227f79b
dkaszyns <01169181@pw.edu.pl>
2022-01-09 12:39:24 +0100

    Separate views logic

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import tickets_home
+from .views import tickets_home, make_reservation
 
 
 urlpatterns = [
     path('', tickets_home, name='home'),
+    path('/reservation', make_reservation, name='make-reservation')
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from django.db import reset_queries
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import messages
+from django.views.decorators.http import require_GET, require_POST
 from .forms import ReservationForm
 from .utils import get_swimlines_info
 from .models import Reservation
@@ -9,21 +11,8 @@ from panel.utils import get_config
 from .exceptions import NoAvailableSwimlaneError, FacilityClosedError
 
 
+@require_GET
 def tickets_home(request):
-    if request.method == 'POST':
-        form = ReservationForm(request.POST)
-
-        if form.is_valid():
-            try:
-                form.save()
-                messages.success(request, 'Reservation made!', extra_tags='success')
-            except (NoAvailableSwimlaneError, FacilityClosedError) as error_msg:
-                messages.error(request, error_msg, extra_tags='danger')
-        else:
-            messages.error(request, 'Reservation form contains errors.', extra_tags='danger')
-
-        return HttpResponseRedirect('/')
-
     config = get_config()
     current_reservations = Reservation.get_overlapping_reservations(datetime.now(),
                                                                     datetime.now())
@@ -31,6 +20,20 @@ def tickets_home(request):
     info = get_swimlines_info(config, current_reservations)[0]
     form = ReservationForm()
 
-    print(info)
-
     return render(request, 'tickets/tickets.html', {'form': form, 'info': info})
+
+
+@require_POST
+def make_reservation(request):
+    form = ReservationForm(request.POST)
+
+    if form.is_valid():
+        try:
+            form.save()
+            messages.success(request, 'Reservation made!', extra_tags='success')
+        except (NoAvailableSwimlaneError, FacilityClosedError) as error_msg:
+            messages.error(request, error_msg, extra_tags='danger')
+    else:
+        messages.error(request, 'Reservation form contains errors.', extra_tags='danger')
+
+    return HttpResponseRedirect('/')
